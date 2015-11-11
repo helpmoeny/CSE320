d4605d51046b3bf429446bbbab4fd550e6eec175
unknown <asdfghjkl1234567890zz@gmail.com>
2015-11-11 15:38:30 -0500

    finished proj09, simplified remain

--- Project 9/proj09.support.s
+++ Project 9/proj09.support.s
@@ -109,6 +109,10 @@ enddivide:
 remain:
 		save        %sp,-96,%sp
 
+		sra         %i0, 31, %l0	!right shift product by 31 bits
+		mov			%l0, %y
+		nop
+		
 		cmp         %i0, 0		!zero divided by anything is zero
 		be         	zero
 		nop
@@ -116,14 +120,6 @@ remain:
 		cmp         %i1, 0		!Dividing by zero is not allowed
 		be          error
 		nop
-		
-		cmp         %i0, 0		!negative first value
-		blt         first_neg_remain
-		nop
-
-		cmp         %i1, 0		!negative second value
-		blt         second_neg_remain
-		nop
 
 		!start actual process of getting the remainder, divide
 		sdivcc      %i0,%i1,%l7
@@ -142,89 +138,6 @@ remain:
 
 		ba          end
 		nop
-
-first_neg_remain:		! first value is negative, second value is positive
-		cmp         %i1, 0
-		be          error
-		nop
-
-		cmp         %i1, 0
-		blt         both_neg_remain	!both values are negative
-		nop
-
-		!signed multiply the first value with negative one
-		smulcc      %i0, -1, %i0
-
-		!start actual process of getting the remainder, divide
-		sdivcc      %i0,%i1,%l7
-		bvs         error
-		nop
-
-		!multiply the quotient
-		smulcc      %l7,%i1,%l7
-		bvs         error
-		nop
-
-		!subtract the quotient from the dividend
-		subcc       %i0,%l7,%i0
-		bvs         error
-		nop
-
-		!multiply the answer by negative 1 to get the correct value
-		smulcc      %i0, -1, %i0
-
-		ba          end
-		nop
-endfirst_neg_remain:
-
-both_neg_remain:		! both values are negative
-
-		smulcc      %i0, -1, %i0
-		smulcc      %i1, -1, %i1
-
-		!start actual process of getting the remainder, divide
-		sdivcc      %i0,%i1,%l7
-		bvs         error
-		nop
-
-		!multiply the quotient
-		smulcc      %l7,%i1,%l7
-		bvs         error
-		nop
-
-		!subtract the quotient from the dividend
-		subcc       %i0,%l7,%i0
-		bvs         error
-		nop
-
-		!multiply the answer by negative 1 to get the correct value
-		smulcc      %i0, -1, %i0
-
-		ba          end
-		nop
-endboth_neg_remain:
-
-second_neg_remain:		! first value is positive, second value is negative
-		smulcc      %i1, -1, %i1
-
-		!start actual process of getting the remainder, divide
-		sdivcc      %i0,%i1,%l7
-		bvs         error
-		nop
-
-		!multiply the quotient
-		smulcc      %l7,%i1,%l7
-		bvs         error
-		nop
-
-		!subtract the quotient from the dividend
-		subcc       %i0,%l7,%i0
-		bvs         error
-		nop
-
-		ba          end
-		nop
-endsecond_neg_remain:
 endremain:
 
 
